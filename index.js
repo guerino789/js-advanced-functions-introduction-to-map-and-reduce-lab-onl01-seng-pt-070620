7e15b327ed6860aa39c407504630d784fb67fcab
guerino antonini <guerantonini94@gmail.com>
2021-02-13 16:52:16 -0500

    commit tree e33469ea978a268521e41f96ea93790a0d86d0fd

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Your code here
+
+// sourceArray => [1,2,3]
+function mapToNegativize(sourceArray){
+    var array = [];
+    for (var i = 0; i < sourceArray.length; i++) {
+        // do something
+        array.push(sourceArray[i] * -1);
+    }
+    // array => [-1,-2,-3]
+    return array;
+}
+
+function mapToNoChange(sourceArray){
+    var array = [];
+    for (var i = 0; i < sourceArray.length; i++){
+        array.push(sourceArray[i]);
+    }
+    
+    return array;
+}
+
+function mapToDouble(sourceArray){
+    var array = [];
+    for (var i = 0; i < sourceArray.length; i++){
+        array.push(sourceArray[i] * 2)
+    }
+    return array;
+}
+
+function mapToSquare(sourceArray){
+    var array = [];
+    for (var i = 0; i < sourceArray.length; i++){
+        array.push(sourceArray[i]**2)
+    }
+    return array;
+}
+
+
+function reduceToTotal(sourceArray, startingPoint = 6){
+    for (var i = 0; i < sourceArray.length; i++){
+        
+    }
+}
